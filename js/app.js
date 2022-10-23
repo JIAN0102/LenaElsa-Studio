6ab02db52d852635c9e0ccd0876966640b3b820a
JL Chen <azx011014@gmail.com>
2022-10-23 21:19:00 +0800

    init

---
+++ js/app.js
@@ -0,0 +1,50 @@
+window.addEventListener("DOMContentLoaded", () => {
+  fetch("./layout/menu.html")
+    .then((response) => {
+      return response.text();
+    })
+    .then((data) => {
+      document.getElementById("menu").innerHTML = data;
+    });
+
+  fetch("./layout/header.html")
+    .then((response) => {
+      return response.text();
+    })
+    .then((data) => {
+      document.getElementById("header").innerHTML = data;
+
+      const toggleTheme = () => {
+        const theme = localStorage.getItem("theme");
+        const htmlClassList = document.documentElement.classList;
+        const isDark = theme === "dark";
+        htmlClassList.toggle("dark", !isDark);
+        htmlClassList.toggle("light", isDark);
+        localStorage.setItem("theme", isDark ? "light" : "dark");
+      };
+      document
+        .getElementById("theme-toggle")
+        .addEventListener("click", toggleTheme, { passive: true });
+
+      document.querySelectorAll(".menu-title").forEach((title) => {
+        title.addEventListener("click", function () {
+          this.parentNode.classList.toggle("is-active");
+        });
+      });
+    });
+
+  const storagedTheme = localStorage.getItem("theme");
+  const htmlClassList = document.documentElement.classList;
+  const DARK = "dark";
+  const LIGHT = "light";
+  const isDark =
+    storagedTheme === DARK ||
+    (!storagedTheme &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches);
+
+  htmlClassList.toggle(LIGHT, !isDark);
+  htmlClassList.toggle(DARK, isDark);
+  if (!storagedTheme) {
+    localStorage.setItem("theme", isDark ? DARK : LIGHT);
+  }
+});
